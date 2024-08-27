f52e7f3e1004f9d1b77d98795c35fe2de82d6141
48810308fir@gmail.com <48810308fir@gmail.com>
2024-08-28 04:34:58 +0700

    tarea persona

---
+++ src/model/Main.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear una instancia de Persona
+        Persona persona = new Persona(1L, "FLORENTINO", "INGA", "RIMACHE", "934380069");
+
+        // Mostrar la información de la persona
+        persona.mostrarInformacion();
+    }
+}
+
---
+++ src/model/Persona.java
@@ -0,0 +1,69 @@
+package model;
+
+public class Persona {
+    private Long id;
+    private String name;
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    // Constructor
+    public Persona(Long id, String name, String firstName, String lastName, String phone) {
+        this.id = id;
+        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // Método para mostrar información de la persona
+    public void mostrarInformacion() {
+        System.out.println("ID: " + id);
+        System.out.println("Nombre: " + name);
+        System.out.println("Primer Nombre: " + firstName);
+        System.out.println("Apellido: " + lastName);
+        System.out.println("Teléfono: " + phone);
+    }
+}
+
